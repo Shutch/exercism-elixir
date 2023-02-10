9f5a4c61d803ec50cf9ad4990ba08748abe19b41
Shutch <shutchinson1190@gmail.com>
2023-02-10 06:15:02 +0000

    Partial add log-parser exercise

---
+++ elixir/log-parser/lib/log_parser.ex
@@ -0,0 +1,17 @@
+defmodule LogParser do
+  def valid_line?(line) do
+    line =~ ~r/^\[DEBUG|INFO|WARN|ERROR\]/
+  end
+
+  def split_line(line) do
+    Regex.run(~r/<(~|\*|=|-)+?>/, line)
+  end
+
+  def remove_artifacts(line) do
+    # Please implement the remove_artifacts/1 function
+  end
+
+  def tag_with_user_name(line) do
+    # Please implement the tag_with_user_name/1 function
+  end
+end
