5b827fc4bb674c89b16b978686e05ef8c5ffbb57
Scott Hutchinson <shutchinson1190@gmail.com>
2023-02-14 19:25:55 +0000

    Add lucas-numbers exercise

---
+++ elixir/lucas-numbers/lib/lucas_numbers.ex
@@ -0,0 +1,20 @@
+defmodule LucasNumbers do
+  @moduledoc """
+  Lucas numbers are an infinite sequence of numbers which build progressively
+  which hold a strong correlation to the golden ratio (φ or ϕ)
+
+  E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
+  """
+  def generate(count) when is_integer(count) and count >= 1 do
+    numbers = case count do
+      1 -> [2]
+      2 -> [2, 1]
+      _ -> Stream.unfold({2, 1}, fn {a, b} -> {a, {b, a + b}} end) |> Enum.take(count)
+    end
+    numbers
+  end
+
+  def generate(_count) do
+    raise ArgumentError, "count must be specified as an integer >= 1"
+  end
+end
